b60f2515348a15da8d7891ef961b91a3221f18e4
Christoph Finkensiep <chfin@chfin.de>
2020-10-03 11:12:47 +0200

    Initial commit

---
+++ nix/default.nix
@@ -0,0 +1,111 @@
+{ system ? builtins.currentSystem
+, crossSystem ? null
+# Lets you customise ghc and profiling (see ./haskell.nix):
+, config ? {}
+# Lets you override niv dependencies of the project without modifications to the source.
+, sourcesOverride ? {}
+, cudaSupport ? false
+, cudaMajorVersion ? null
+}:
+
+# assert that the correct cuda versions are used
+assert cudaSupport -> (cudaMajorVersion == "9" || cudaMajorVersion == "10");
+
+let
+  sources = import ./sources.nix { inherit pkgs; }
+    // sourcesOverride;
+  iohKNix = import sources.iohk-nix {};
+  haskellNix = (import sources."haskell.nix" { inherit system sourcesOverride; }).nixpkgsArgs;
+  # use our own nixpkgs if it exist in our sources,
+  # otherwise use iohkNix default nixpkgs.
+  nixpkgs = sources.nixpkgs-2003 or
+    (builtins.trace "Using IOHK default nixpkgs" iohKNix.nixpkgs);
+
+  # for inclusion in pkgs:
+  overlays =
+    # Haskell.nix (https://github.com/input-output-hk/haskell.nix)
+    haskellNix.overlays
+    # override Haskell.nix hackage and stackage sources
+    ++ [
+      (pkgsNew: pkgsOld: let inherit (pkgsNew) lib; in {
+        haskell-nix = pkgsOld.haskell-nix // {
+          hackageSrc = sources.hackage-nix;
+          stackageSrc = sources.stackage-nix;
+          custom-tools = pkgsOld.haskell-nix.custom-tools // {
+            haskell-language-server."0.4.0" = args:
+              (pkgsOld.haskell-nix.cabalProject (args // {
+                name = "haskell-language-server";
+                src = pkgsOld.fetchFromGitHub {
+                  owner = "haskell";
+                  repo = "haskell-language-server";
+                  rev = "0a18edde24923251a148cbbc0ae993a6aac83b9c";
+                  sha256 = "0b94l6bywa6jk20y2cswyq5ks4g515895k2apvr1mdfkfhngdb7b";
+                  fetchSubmodules = true;
+                };
+                # lookupSha256 = { location, tag, ... } : {
+                #   "https://github.com/wz1000/shake"."fb3859dca2e54d1bbb2c873e68ed225fa179fbef" = "0sa0jiwgyvjsmjwpfcpvzg2p7277aa0dgra1mm6afh2rfnjphz8z";
+                #   "https://github.com/peti/cabal-plan"."894b76c0b6bf8f7d2f881431df1f13959a8fce87" = "06iklj51d9kh9bhc42lrayypcpgkjrjvna59w920ln41rskhjr4y";
+                #   }."${location}"."${tag}";
+                # plan-sha256 = "0a6c4lhnlm2lkic91ips0gb3hqlp3fk2aa01nsa8dhz9l8zg63da";
+                modules = [{
+                  # Tests don't pass for some reason, but this is a somewhat random revision.
+                  packages.haskell-language-server.doCheck = false;
+                }];
+              })).haskell-language-server.components.exes.haskell-language-server;
+          };
+        };
+      })
+    ]
+    # the haskell-nix.haskellLib.extra overlay contains some useful extra utility functions for haskell.nix
+    ++ iohKNix.overlays.haskell-nix-extra
+    # the iohkNix overlay contains nix utilities and niv
+    ++ iohKNix.overlays.iohkNix
+    # libtorch overlays from pytorch-world
+    # TODO: pull in libGL_driver and cudatoolkit as done in https://github.com/NixOS/nixpkgs/blob/master/pkgs/games/katago/default.nix
+    ++ [
+      (pkgs: _: with pkgs;
+        let libtorchSrc = callPackage "${sources.pytorch-world}/libtorch/release.nix" { }; in
+        if cudaSupport && cudaMajorVersion == "9" then
+          let libtorch = libtorchSrc.libtorch_cudatoolkit_9_2; in
+          {
+            c10 = libtorch;
+            torch = libtorch;
+            torch_cpu = libtorch;
+            torch_cuda = libtorch;
+          }
+        else if cudaSupport && cudaMajorVersion == "10" then
+          let libtorch = libtorchSrc.libtorch_cudatoolkit_10_2; in
+          {
+            c10 = libtorch;
+            torch = libtorch;
+            torch_cpu = libtorch;
+            torch_cuda = libtorch;
+          }
+        else
+          let libtorch = libtorchSrc.libtorch_cpu; in
+          {
+            c10 = libtorch;
+            torch = libtorch;
+            torch_cpu = libtorch;
+          }
+      )
+    ]
+    # our own overlays:
+    ++ [
+      (pkgs: _: with pkgs; {
+        # commonLib: mix pkgs.lib with iohk-nix utils and sources:
+        commonLib = lib // iohkNix
+          // import ./util.nix { inherit haskell-nix; }
+          # also expose sources, nixpkgs, and overlays
+          // { inherit overlays sources nixpkgs; };
+      })
+      # haskell-nix-ified hasktorch-skeleton cabal project:
+      (import ./pkgs.nix)
+    ];
+
+  pkgs = import nixpkgs {
+    inherit system crossSystem overlays;
+    config = haskellNix.config // config;
+  };
+
+in pkgs
